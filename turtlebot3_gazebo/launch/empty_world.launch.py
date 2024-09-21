a984748817403a7948c209a9c3664d7c46da2b63
Jack Yang <jack.v.yang@gmail.com>
2024-09-22 03:50:09 +1200

    world

--- turtlebot3_gazebo/launch/empty_world.launch.py
+++ turtlebot3_gazebo/launch/empty_world.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'empty_world.world'
+        'turtlebot3_dqn_stage1.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
